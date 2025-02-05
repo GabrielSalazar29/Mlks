2132690b72e4d5e9dee2c7d5a422264ec1f35508
Filipe <filipepinho2010@hotmail.com>
2025-02-05 03:20:05 -0300

    feat: removendo mecanica de pulo

--- Assets/MoveController.cs
+++ Assets/MoveController.cs
@@ -1,30 +1,36 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class MoveController : MonoBehaviour {
 	public int speed;
 	private float rotacaoX;
+	private float rotacaoY;
+	private Vector2 moveInput;
+	private Vector2 lookInput;
+	public CharacterController characterController;
+
+	public void OnMove(InputValue value)
+	{
+		moveInput = value.Get<Vector2>();
+	}
+    public void OnLook(InputValue value)
+    {
+        lookInput = value.Get<Vector2>();
+    }
 
 
-	private void Start() {
+    private void Start() {
 		Cursor.lockState = CursorLockMode.Locked;
+		
 	}
 	// Update is called once per frame
 	void Update() {
-		if (Input.GetKey(KeyCode.W))
-			transform.position += speed * Time.deltaTime * Vector3.forward;
-
-		if (Input.GetKey(KeyCode.S))
-			transform.position += speed * Time.deltaTime * Vector3.back;
-
-		if (Input.GetKey(KeyCode.A))
-			transform.position += speed * Time.deltaTime * Vector3.left;
-
-		if (Input.GetKey(KeyCode.D))
-			transform.position += speed * Time.deltaTime * Vector3.right;
-
+		transform.Translate(moveInput.x * speed * Time.deltaTime, 0f, moveInput.y * speed * Time.deltaTime);		
+		//characterController.Move(new Vector3(moveInput.x * speed * Time.deltaTime, 0f, moveInput.y * speed * Time.deltaTime));
 
 		rotacaoX += Input.GetAxis("Mouse X");
+		rotacaoY += Input.GetAxis("Mouse Y");
 
-		transform.rotation = Quaternion.Euler(0f, rotacaoX, 0f);
+		transform.rotation = Quaternion.Euler(-Mathf.Clamp(rotacaoY, -80, 80), rotacaoX, 0f);
 	}
 }
